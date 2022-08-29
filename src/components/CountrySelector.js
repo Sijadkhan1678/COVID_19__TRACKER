c11c02ec4624c4955efb891d4ea4d69fccd6a0c9
Sijadkhan1678 <sijadkhan1678@gmail.com>
2022-08-29 22:32:18 +0500

    Updated CountrySelector component

--- src/components/CountrySelector.js
+++ src/components/CountrySelector.js
@@ -1,24 +1,26 @@
 import React,{useState} from 'react';
+import Typography from '@mui/material/Typography'
 import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
 import Select from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
 import Stack from '@mui/material/Stack'
 
-const CountrySelctor = () =>{
+const CountrySelector = () => {
   
 const [country,setcountry]= useState("global")
 
 const handleChange = (e) =>{
 setcountry(e.target.value)
-
+ 
 }
 
 return(
-<Stack mt={1}  direction='flex' justifyContent="center">
-<Typography > Selct country</Typography
-<FormControl sx={{width:'50%'}}>
-<InputLabel id="demo-simple-select-label">Select Country</InputLabel>
+  
+<Stack mt={3}  direction='column' justifyContent="center">
+<Typography variant='h6' sx={{textAlign:'center',mb:4,fontWeight:400}} > Selct country</Typography>
+<FormControl sx={{width:'50%' ,alignSelf:'center'}}>
+<InputLabel id="demo-simple-select-label" mb='5'>Select Country</InputLabel>
 <Select
 labelId="demo-simple-select-label"
 id="demo-simple-select"
@@ -26,14 +28,14 @@ value={country}
 onChange={handleChange}
 >
 <MenuItem value={country}>Global</MenuItem>
-<MenuItem value='india'>Ten</MenuItem>
-<MenuItem value='pakistan'>Twenty</MenuItem>
-<MenuItem value='usa'>Thirty</MenuItem>
+<MenuItem value='india'>India</MenuItem>
+<MenuItem value='pakistan'>pakistan</MenuItem>
+<MenuItem value='japan'>japan</MenuItem>
 </Select>
 </FormControl>
-{country}
+
 </Stack>
 )
 }
 
-export default CountrySelctor;,
+export default CountrySelector;
