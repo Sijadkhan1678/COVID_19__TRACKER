301c92f50692b090f8b0212e7372a55a5442a048
Sijadkhan1678 <sijadkhan1678@gmail.com>
2022-09-07 21:15:15 +0500

    Improved Loading component

--- src/components/layout/Loading.js
+++ src/components/layout/Loading.js
@@ -1,10 +1,24 @@
 import React from 'react';
-
+import loading from './loading.gif'
+import Box from '@mui/material/Box'
 const Loading = () => {
  
  return (
-      <div>  loading...  </div>
+      <Box sx={{
+         width: 200,
+         height : 100,
+         mx: 'auto',
+         mt: 5,
+        }}>  
+      <img style={style} src={loading} alt='loading' />
+      </Box>
    )
  
 }
+const style = {
+  display: 'block',
+  width:'120px',
+  margin: '0 auto'
+}
+
 export default Loading
